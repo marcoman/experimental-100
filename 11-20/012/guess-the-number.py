4a1f703cc6514d317aa7a2a4abea21edf7d0f9da
Marco Morales <marco@sportsensesay.com>
2023-06-12 20:19:34 -0400

    convert program to functions

--- 11-20/012/guess-the-number.py
+++ 11-20/012/guess-the-number.py
@@ -22,38 +22,47 @@ TOO_LOW = "Too low."
 def generate_random_number():
     return random.randint(1, 100)
 
-playing = True
-
-print(WELCOME_MESSAGE)
-
-level = input("Choose a difficulty. Type 'easy' or 'hard': ")
-if level == "easy":
-    attempts = 10
-elif level == "hard":
-    attempts = 5
-else:
-    print("Invalid input")
-    playing = False
-
-print(INSTRUCTIONS)
-
-random_number = generate_random_number()
-
-while playing:
-    if attempts == 0:
-        print(f"You've run out of guesses, you lose.  The correct number was {random_number}")
-        playing = False
-        break
-
-    print(f"You have {attempts} attempts remaining to guess the number.")
-    guess = int(input(CHOOSE_NUMBER))
-    if guess == random_number:
-        print("You got it!")
-        playing = False
-        break
-    elif guess > random_number:
-        print(TOO_HIGH)
+def get_difficulty():
+    attempts = 0
+    level = input("Choose a difficulty. Type 'easy' or 'hard': ")
+    if level == "easy":
+        attempts = 10
+    elif level == "hard":
+        attempts = 5
     else:
-        print(TOO_LOW)
-    attempts -= 1
+        print("Invalid input")
+    return attempts
+
+def play_game():
+    print(WELCOME_MESSAGE)
+    print(INSTRUCTIONS)
+
+    random_number = generate_random_number()
+    attempts = get_difficulty()
+    if attempts > 0:
+        playing = True
+    else:
+        playing = False
+
+    while playing:
+        if attempts == 0:
+            print(f"You've run out of guesses, you lose.  The correct number was {random_number}")
+            playing = False
+            break
+
+        print(f"You have {attempts} attempts remaining to guess the number.")
+        guess = int(input(CHOOSE_NUMBER))
+        if guess == random_number:
+            print("You got it!")
+            playing = False
+            break
+        elif guess > random_number:
+            print(TOO_HIGH)
+        else:
+            print(TOO_LOW)
+        attempts -= 1
+
+
+
+play_game()
 
