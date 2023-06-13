0e7c8767d00ef57ebf2d813a1a4accaa0497b67d
Marco Morales <marco@sportsensesay.com>
2023-06-12 20:09:24 -0400

    add some more text and a leading comment block.

--- 11-20/012/guess-the-number.py
+++ 11-20/012/guess-the-number.py
@@ -1,3 +1,14 @@
+"""
+Number Guessing Game Objectives:
+ Include an ASCII art logo.
+ Allow the player to submit a guess for a number between 1 and 100.
+ Check user'XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX"Too high." or "Too low." depending on the user's answer.
+ If they got the answer wrong, show the correct answer to the player.
+ Track the number of turns remaining.
+ If they run out of turns, provide feedback to the player.
+ Include two different difficulty levels (e.g., 10 guesses in easy mode, only 5 guesses in hard mode).
+"""
+
 import random
 
 WELCOME_MESSAGE = "Welcome to the Number Guessing Game!"
@@ -30,7 +41,7 @@ random_number = generate_random_number()
 
 while playing:
     if attempts == 0:
-        print("You've run out of guesses, you lose.")
+        print(f"You've run out of guesses, you lose.  The correct number was {random_number}")
         playing = False
         break
 
