140c3bbc9ce574508950d74616f81e114b736038
Marco Morales <marco@sportsensesay.com>
2023-06-14 18:32:30 -0400

    checkpoint for crossy turtle.

--- 21-30/23/main.py
+++ 21-30/23/main.py
@@ -4,11 +4,29 @@ from player import Player
 from car_manager import CarManager
 from scoreboard import Scoreboard
 
+SCREEN_WIDTH = 600
+SCREEN_HEIGHT = 600
+
 screen = Screen()
-screen.setup(width=600, height=600)
+screen.setup(width=SCREEN_WIDTH, height=SCREEN_HEIGHT)
 screen.tracer(0)
+
+player = Player()
+car_manager = CarManager()
+scoreboard = Scoreboard()
 
 game_is_on = True
 while game_is_on:
     time.sleep(0.1)
+    car_manager.create_car()
+    if car_manager.collision(player):
+        scoreboard.game_over()
+        game_is_on = False
+    if player.is_at_finish_line():
+        player.go_to_start()
+        car_manager.level_up()
+        scoreboard.increase_level()
+    else:
+        car_manager.move_cars()
+    scoreboard.update_scoreboard()
     screen.update()
--- 21-30/23/player.py
+++ 21-30/23/player.py
@@ -1,7 +1,30 @@
+from turtle import Turtle
+from turtle import Screen
 STARTING_POSITION = (0, -280)
 MOVE_DISTANCE = 10
 FINISH_LINE_Y = 280
 
 
-class Player:
-    pass
+class Player (Turtle):
+
+    def __init__(self):
+        super().__init__()
+        self.shape("turtle")
+        self.penup()
+        self.go_to_start()
+        self.setheading(90)
+        self.screen = Screen()
+        self.screen.listen()
+        self.screen.onkey(self.go_up, "Up")
+
+    def go_up(self):
+        self.forward(MOVE_DISTANCE)
+
+    def go_to_start(self):
+        self.goto(STARTING_POSITION)
+
+    def is_at_finish_line(self):
+        if self.ycor() > FINISH_LINE_Y:
+            return True
+        else:
+            return False
