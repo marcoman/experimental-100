f0b846ef4fe1a10274c28a5379896bb579dbdc4f
Marco Morales <marco@sportsensesay.com>
2023-07-06 15:07:11 -0400

    finish the assignment.

--- 31-40/37/test.py
+++ 31-40/37/test.py
@@ -18,39 +18,53 @@ user_params = {
     'notMinor': 'yes'
     }
 
-
-print (user_params)
-
-# myrequest = requests.post(url=PIXELA_USER_API, json=user_params)
-# print (myrequest.text)
-# print (myrequest.status_code)
-
 graph_params = {
     'id': 'code-graph-1',
     'name': 'Coding Graph',
     'unit': 'commit',
     'type': 'int',
     'color': 'ajisai'
-}
+    }
 
 graph_headers = {
     'X-USER-TOKEN': PIXELA_TOKEN
     }
 
 
+def setup_account () :
+    print (user_params)
 
-# myrequest = requests.post(url=PIXELA_GRAPHS_API, json=graph_params, headers=graph_headers)
-# print (myrequest.text)
-# print (myrequest.status_code)
-# print (myrequest.text)
+    myrequest = requests.post(url=PIXELA_USER_API, json=user_params)
+    print (myrequest.text)
+    print (myrequest.status_code)
+
+
+def create_graph() :
+    myrequest = requests.post(url=PIXELA_GRAPHS_API, json=graph_params, headers=graph_headers)
+    print (myrequest.text)
+    print (myrequest.status_code)
+    print (myrequest.text)
+
+
+def update_graph(update_body) :
+    myrequest = requests.post(url=PIXELA_GRAPHS_API + '/code-graph-1', json=update_body, headers=graph_headers)
+    print (myrequest.text)
+    print (myrequest.status_code)
 
 today = str (dt.datetime.now().strftime('%Y%m%d'))
+yesterday = (dt.datetime.now() - dt.timedelta(days=1)).strftime('%Y%m%d')
 
-graph_update = {
+graph_today = {
     'date': today,
     'quantity': '10'
 }
 
-myrequest = requests.post(url=PIXELA_GRAPHS_API + '/code-graph-1', json=graph_update, headers=graph_headers)
-print (myrequest.text)
-print (myrequest.status_code)
+graph_yesterday = {
+    'date': yesterday,
+    'quantity': '20'
+}
+
+# setup_account()
+# create_graph()
+update_graph(graph_today)
+update_graph(graph_yesterday)
