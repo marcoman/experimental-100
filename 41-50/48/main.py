e4e8c57ed313b4a182a66e915dfd019c5d3c51eb
Marco Morales <mmorales@gradle.com>
2023-09-10 15:56:58 -0400

    checkpoint

--- 41-50/48/main.py
+++ 41-50/48/main.py
@@ -1,6 +1,19 @@
 
 # Created on Wed Apr  3 15:53:10 2019
+from selenium import webdriver
 
-
+chrome_options = webdriver.ChromeOptions()
+chrome_options.add_experimental_option("detach", True)
 
 chrome_driver = "~/bin/chromedriver"
+
+driver = webdriver.Chrome(options=chrome_options)
+#driver = webdriver.Edge()
+driver.get("https://google.com")
+
+
+# This closes the active tab
+#driver.close()
+
+# This quits the entire browser.
+#driver.quit()
