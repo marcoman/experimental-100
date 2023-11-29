e9c51fe3aa8f69b48637cf77551c36bd54b51ae4
Marco Morales <marco@sportsensesay.com>
2023-11-29 15:49:10 -0500

    add some explanation.

--- 51-60/55/hello.py
+++ 51-60/55/hello.py
@@ -2,6 +2,11 @@ from flask import Flask
 from functools import wraps
 app = Flask(__name__)
 
+# These links helped me understand what was going on.
+# https://realpython.com/primer-on-python-decorators/#both-please-but-never-mind-the-bread
+# This one especially, since it helped explain how setting the default of function to None is a sensible strategy to make things work
+# https://stackoverflow.com/questions/653368/how-to-create-a-decorator-that-can-be-used-either-with-or-without-parameters
+
 # This next line will return "__name__" if we run this directly, and not as a library.
 # print (__name__)
 
