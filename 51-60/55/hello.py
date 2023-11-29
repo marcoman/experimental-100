01078d4d78a4132b9887dce25b9423469af5dc6e
Marco Morales <marco@sportsensesay.com>
2023-11-29 14:44:27 -0500

    decorators work for flask function with and without variables

--- 51-60/55/hello.py
+++ 51-60/55/hello.py
@@ -1,45 +1,67 @@
 from flask import Flask
+from functools import wraps
 app = Flask(__name__)
 
 # This next line will return "__name__" if we run this directly, and not as a library.
 # print (__name__)
 
 # This is my bold decorator
-def dec_b(function):
-    def b():
-        return f"<b>{function()}</b>"
-    return b
 
-def dec_i(function):
-    def i():
-        return f"<i>{function()}</i>"
-    return i
+def db(function=None):
+    def wrapper(func):
+        @wraps(func)
+        def wrap(*args, **kwargs):
+            result = f'<b>{func(*args, **kwargs)}</b>'
+            return result
+        return wrap
+    if function:
+        return wrapper(function)
+    else:
+        return wrapper
 
-def dec_u(function):
-    def u():
-        return f"<u>{function()}</u>"
-    return u
+def dem(function=None):
+    def wrapper(func):
+        @wraps(func)
+        def wrap(*args, **kwargs):
+            result = f'<em>{func(*args, **kwargs)}</em>'
+            return result
+        return wrap
+    if function:
+        return wrapper(function)
+    else:
+        return wrapper
+
+def du(function=None):
+    def wrapper(func):
+        @wraps(func)
+        def wrap(*args, **kwargs):
+            result = f'<u>{func(*args, **kwargs)}</u>'
+            return result
+        return wrap
+    if function:
+        return wrapper(function)
+    else:
+        return wrapper
 
 @app.route('/')
-@dec_b()
-@dec_i()
+@db
+@du
 def hello_world():
-    return 'Hello World.'
-
+    return ('Hello World.')
 
 @app.route('/bye')
-@dec_i()
-@dec_u()
+@du
 def bye():
-    return 'Goodbye!'
+    return ('Goodbye!')
 
 @app.route('/greet/<name>')
-@dec_i()
 def greet(name):
     return(f"Hello my friend {name}")
 
 @app.route('/<name>')
-@dec_u()
+@dem
+@db
+@du
 def name(name):
     return(f"Hello my best friend, {name}")
 
