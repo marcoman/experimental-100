2d1d7ec6019d3b246d0d3dbf2171ef65763ef988
Marco Morales <marco@sportsensesay.com>
2023-11-29 20:29:17 -0500

    number resets when we navigate to the root level

--- 51-60/55/server.py
+++ 51-60/55/server.py
@@ -1,12 +1,15 @@
 from functools import wraps
 from flask import Flask
 import random
+
 app = Flask(__name__)
 
 GIF_DEFAULT = "https://media.giphy.com/media/3o7aCSPqXE5C6T8tBC/giphy.gif"
 GIF_HIGHER = "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX"
 GIF_LOWER = "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX"
 
+global number
+number = random.randint(0, 9)
 color = []
 n = 100
 for i in range(n):
@@ -24,6 +27,17 @@ def add_h1(function=None):
     else:
         return wrapper
 
+def add_h2(function=None):
+    def wrapper(func):
+        @wraps(func)
+        def wrap(*args, **kwargs):
+            result = f'<h2>{func(*args, **kwargs)}</h2>'
+            return result
+        return wrap
+    if function:
+        return wrapper(function)
+    else:
+        return wrapper
 
 def add_gif(function=None):
     def wrapper(func):
@@ -57,29 +71,33 @@ def add_gif2(function):
         print(result)
     return wrapper
 
-
-def write_report(guess, number):
-    output = "You guessed, {guess}"
+@add_h2
+def write_report(guess):
+    output = f"You guessed, {guess}"
+    # output += f"<br>and the number is {number}"
     if guess > number:
         output += " and it was too high"
     elif guess < number:
         output += " and it was too low"
     else:
-        output += " and it was correct"
+        output += " and it was correct!"
+    return (output)
 
 @app.route('/')
 @add_h1
 def hello_world():
+    global number
+    number = random.randint(0, 9)
+    print (f"Number is {number}")
     return ('Guess a number between 0 and 9.')
 
-
-# TODO: Make this look for an integer
-@app.route('/<number>')
+@app.route('/<int:guess>')
 @add_random_color
 def number(guess):
-    return(f"{write_report(guess, number)}")
+    return(f"{write_report(guess)}")
 
 number = random.randint(0, 9)
+print (f"Number is {number}")
 
 if __name__ == "__main__":
     app.run(debug=True)
