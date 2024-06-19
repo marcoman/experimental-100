4008d9bf6ed02b909bd431af5050a577f9382502
Marco Morales <marco@sportsensesay.com>
2024-06-19 11:14:58 -0400

    point check.

---
+++ 51-60/60/html-forms/main.py
@@ -0,0 +1,24 @@
+from flask import Flask
+from flask import render_template
+import requests
+from flask import request
+
+app = Flask(__name__)
+
+# Read json file
+headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:50.0) Gecko/20100101 Firefox/50.0'}
+
+@app.route('/')
+def hello():
+    return render_template('index.html')
+
+# Accept the name and password via post into the file submit.html
+@app.route('/submit.html', methods=['POST'])
+def submit():
+    name = request.form['name']
+    password = request.form['password']
+    return render_template('submit.html', name=name, password=password)
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
