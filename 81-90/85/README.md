5d4f7462b9476e313318a233212f36aff86127be
Marco Morales <marco@sportsensesay.com>
2024-08-02 16:47:18 -0400

    checkpoint.
    I can add a watermark to images, after converting them to RGP format.
    TODO: tidy and finish up.

---
+++ 81-90/85/README.md
@@ -0,0 +1,31 @@
+# Overview
+
+This is the day 85 assignment.
+The goal is to build a desktop application that lets us load an image, add a watermark, and then save the image.
+
+_From the course:_
+
+Using what you have learnt about Tkinter, you will create a desktop application with a Graphical User Interface (GUI) where you can upload an image and use Python to add a watermark logo/text.
+
+Normally, you would have to use an image editing software like Photoshop to add the watermark, but your program is going to do it automatically.
+
+Use case: e.g you want to start posting your photos to Instagram but you want to add your website to all the photos, you can now use your software to add your website/logo automatically to any image.
+
+A similar online service is: https://watermarkly.com/
+
+
+My comments:
+
+- See my activities for Days 27, 28, 29, 30, 31, 34
+
+# Running
+
+# External Links
+- [project Pillow](https://pypi.org/project/Pillow/)
+- [tkinter library](https://docs.python.org/3/library/tkinter.html)
+
+# requirements.txt
+
+# Final Solution
+
+# TODOs
---
+++ 81-90/85/start/main.py
@@ -0,0 +1,83 @@
+import tkinter as tk
+from PIL import Image, ImageTk, ImageFont, ImageDraw
+from pathlib import Path
+import sys
+
+from tkinter import filedialog
+
+
+# Start by creating a window with a layout for title, the button to upload an image, a label to show the current path, and an image display
+class PictureWatermark:
+
+    # setup the class 
+    def __init__(self):
+        self.mywindow = tk.Tk()
+        self.mywindow.title("Watermark challenge")
+        self.mywindow.minsize(width=500, height=500)
+        self.mywindow.config(padx=20, pady=20)
+
+        self.label = tk.Label(text="No file selected")
+        self.label.grid(column=0, row=0)
+
+        self.button = tk.Button(text="Select image", command=self.select_image)
+        self.button.grid(column=1, row=1)
+
+        self.button = tk.Button(text="Add Watermark", command=self.add_watermark)
+        self.button.grid(column=1, row=2)
+
+        self.canvas = tk.Canvas()
+        self.canvas.grid(column=0, row=2)
+        self.filename = ''
+
+        # self.myimage = tk.PhotoImage(file="./card_front.png")
+        # self.myimage = tk.PhotoImage(file="./ubuntu14.jpg")
+        self.myimage = ImageTk.PhotoImage(Image.open("./ubuntu14.jpg"))
+        self.canvas.config(width=self.myimage.width(), height=self.myimage.height())
+        self.canvas.create_image(0, 0, anchor=tk.NW, image=self.myimage)
+
+        self.counter = 0
+
+    def run(self):
+        self.mywindow.mainloop()
+
+    def select_image(self):
+        print("pressed button")
+        self.counter += 1
+        self.label.config(text=self.counter)
+        self.filename = filedialog.askopenfilename(
+            filetypes={
+                ("image files", "*.jpg;*.png;*.gif"),
+                ("all files", "*.*")
+            },
+            initialdir="./"
+
+        )
+        print(f'Selected {self.filename}')
+        if self.filename:
+            self.display_image(self.filename)
+
+    def display_image(self, filename):
+        self.myimage = ImageTk.PhotoImage(Image.open(filename))
+        self.filename = filename
+        self.canvas.config(width=self.myimage.width(), height=self.myimage.height())
+        self.canvas.create_image(0, 0, anchor=tk.NW, image=self.myimage)
+
+
+    def add_watermark(self):
+        print("add watermark")
+        with Image.open(self.filename) as im:
+            rgb_im = im.convert('RGB')
+            # original_image = Image.open(self.filename)
+            watermark_text = "Marco Watermark"
+            text_position = (50,50)
+            font = ImageFont.load_default()
+            draw = ImageDraw.Draw(rgb_im)
+            draw.text(text = watermark_text, 
+                    xy=text_position,
+                    fill=(255, 255, 255),
+                    font=font)
+            rgb_im.save("watermarked_image.jpg")
+        self.display_image("watermarked_image.jpg")
+
+myPicture = PictureWatermark()
+myPicture.run()
