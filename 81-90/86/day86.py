9f395f5e9fd18ca573c95f3163d1b321c203cbd6
Marco Morales <marco@sportsensesay.com>
2024-08-04 16:34:38 -0400

    done.  Proof of concept works.

--- 81-90/86/day86.py
+++ 81-90/86/day86.py
@@ -6,31 +6,97 @@ ONE_SECOND = 1000
 
 
 class WordGenerator:
+    '''
+    Helper class to generate words or sentences and return them in a list.  
+    '''
     def __init__(self):
         pass
 
     @staticmethod
     def generate_word():
+        '''
+        Generate one word.
+        '''
         return wonderwords.RandomWord().word(include_parts_of_speech=["nouns"]).lower()
     
     @staticmethod
-    def generate_words(num_words):
+    def generate_words(num_words:int):
+        '''
+        Generate the number of words specified by num_words.
+        '''
         return [wonderwords.RandomWord().word(include_parts_of_speech=["nouns"]).lower() for _ in range(num_words)]
     
     @staticmethod
     def generate_sentence():
+        '''
+        Generate one sentence.
+        '''
         return wonderwords.RandomSentence().sentence().lower()
     
     @staticmethod
-    def generate_sentences(num_sentences):
+    def generate_sentences(num_sentences:int):
+        '''
+        Generate the number of sentences specified by num_sentences.
+        '''
         return [wonderwords.RandomSentence().sentence().lower() for _ in range(num_sentences)]
 
 class TypingTest:
+    '''
+    This is the all-in-one class that displays our text and shows our results.
+    We initialize the class, and then run() it to do our WPM.
+    It is a simple class, to prove the concept.
+    '''
 
     def __init__(self) -> None:
         self.mywords =  WordGenerator.generate_words(200)
         self.mysentences = WordGenerator.generate_sentences(200)
 
+        self.reset_stats()
+
+        # Define the application window and layout
+        self.mywindow = tk.Tk()
+
+        self.mywindow.title("Typing Challenge")
+        self.mywindow.minsize(width=500, height=500)
+        self.mywindow.config(padx=20, pady=20)
+
+        # First row - The instructions
+        self.l_instructions = tk.Label(text="Press 'Start' button to begin",
+                                            anchor="w", justify=tk.LEFT
+                                       )
+        self.button = tk.Button(text="Start", command=self.toggle_timer)
+        self.l_instructions.grid(row=0, column=0)
+        self.button.grid(row=0, column=1, padx=10, pady=10)
+
+        # Second row - The text prompt and button
+        self.l_prompt = tk.Label(text="This is what you type:",
+                                 anchor="w", justify=tk.LEFT
+                                 )
+        self.l_prompt_example = tk.Label(text="Test Text", justify=tk.LEFT)
+        self.l_prompt.grid(row=1, column=0, padx=10, pady=10)
+        self.l_prompt_example.grid(row=1, column=1, padx=10, pady=10)
+
+        # Third row - The text we type in
+        self.l_test_instructions = tk.Label(text="Enter your text here:",
+                                            anchor="w", justify=tk.LEFT
+                                            )
+        self.l_test = tk.Label(text="<YOUR INPUT IS SHOWN HERE.", justify=tk.LEFT)
+        self.l_test_instructions.grid(row=2, column=0, padx=10, pady=10)
+        self.l_test.grid(row=2, column=1, padx=10, pady=10)
+
+        # Fourth row - our running stats
+        self.l_stats = tk.Label(text=f"Time={self.elapsed_seconds}" + 
+                              f"\nWPM={self.wpm}" +
+                              f"\nCorrect={self.correct_words}" +
+                              f"\nIncorrect={self.incorrect_words}",
+                              anchor="w", justify= tk.LEFT
+                              )
+        self.l_stats.grid(row=3, column=0)
+
+        # Accept keyboard input
+        self.mywindow.bind('<Key>', self.key_handler)
+
+    def reset_stats(self):
         self.timer_on = False
         self.timer = None
 
@@ -44,56 +110,26 @@ class TypingTest:
         self.total_words = 0
         self.wpm = 0
 
-        # Define the application window and layout
-        self.mywindow = tk.Tk()
-
-        self.mywindow.title("Typing Challenge")
-        self.mywindow.minsize(width=500, height=500)
-        self.mywindow.config(padx=20, pady=20)
-
-        # First row - The instructions
-        self.l_instructions = tk.Label(text="Press 'Start' button to begin")
-        self.button = tk.Button(text="Start", command=self.toggle_timer)
-        self.l_instructions.grid(row=0, column=0)
-        self.button.grid(row=0, column=1, padx=10, pady=10)
-
-        # Second row - The text prompt and button
-        self.l_prompt = tk.Label(text="This is what you type:")
-        self.l_prompt_example = tk.Label(text="Test Text")
-        self.l_prompt.grid(row=1, column=0, padx=10, pady=10)
-        self.l_prompt_example.grid(row=1, column=1, padx=10, pady=10)
-
-        # Third row - The text we type in
-        self.l_test_instructions = tk.Label(text="Enter your text here:")
-        self.l_test = tk.Label(text="<YOUR INPUT IS SHOWN HERE.")
-        self.l_test_instructions.grid(row=2, column=0, padx=10, pady=10)
-        self.l_test.grid(row=2, column=1, padx=10, pady=10)
-
-        # Fourth row - our running stats
-        self.l_stats = tk.Label(text=f"Time={self.elapsed_seconds}" + 
-                              f"\nWPM={self.wpm}" +
-                              f"\nCorrect={self.correct_words}" +
-                              f"\nIncorrect={self.incorrect_words}",
-                              anchor="w", justify="left"
-                              )
-        self.l_stats.grid(row=3, column=0)
-
-        # Accept keyboard input
-        self.mywindow.bind('<Key>', self.key_handler)
- 
     def run(self):
+        '''
+        Run the application and show it onscreen.
+        '''
         self.mywindow.mainloop()
 
     def toggle_timer(self):
         # IF the timer is not on, turn it on and start the timer
         if not self.timer_on:
+            self.reset_stats()
             self.start_timer()
+            self.button.config(text="Stop")
         else:
             self.stop_timer()
+            self.button.config(text="Start")
 
     def start_timer(self):
         print("Timer started")
         self.timer_on = True
+        self.elapsed_seconds = 0
         self.update_test()
         self.update_timer()
 
@@ -174,7 +210,7 @@ class TypingTest:
         else:
             self.wpm = 0
         self.l_stats.config(text=f"Time={self.elapsed_seconds}" +
-                              f"\nWPM={self.wpm}" +
+                              f"\nWPM={self.wpm:.1f}" +
                               f"\nCorrect={self.correct_words}" +
                               f"\nIncorrect={self.incorrect_words}",
                               anchor="w", justify="left"
