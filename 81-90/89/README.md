33b402ecf7aee45b8e8397c882d3593cbbd21536
Marco Morales <marco@sportsensesay.com>
2024-08-08 15:42:20 -0400

    day 89 start.

---
+++ 81-90/89/README.md
@@ -0,0 +1,66 @@
+# Overview
+
+This is the day 89 assignment.
+
+## _From the course:_
+Build a todo list website.
+
+Today, you are going to build a todo list website. This is a right of passage for any developer.
+
+You can choose the type of todo list you want to build. It could be as simple as a website where you can list some items and cross them out. Or as complex as a Kanban-style task list like Trello.
+
+Here is a website for inspiration:
+
+https://flask.io/new
+
+Top Secret: Most developer jobs will be interviewed by someone who is a manager. The top piece of technology a manager uses is a task-manager like Trello or Jira. If you can build a good task-manager, you will definitely impress your future interviewer!
+
+
+## My comments:
+
+Building a TODO list is something many people think about.  I use Microsoft's To-do because it is appoximately what I need to use. 
+
+I'm expecting a few things to add to the TODO app.
+- a landing page that shows my current list.
+    - This page should allow for direct entry.  As simple as "start typing on this line."
+- links on each to edit, mark complete, or delete.
+- routes for `add` `edit` `delete` and `mark complete`.  These sound like the basic CRUD operations and their API-based methods.
+- pretty screens to display
+    - all open tasks
+    - edit page
+    - details of a single task
+- a TODO item should contain the following information:
+    - The title
+    - The due date
+    - The date entered
+    - a Note field
+    - a category
+
+We can display a sortable list with this HTML:
+
+```html
+    <div class="container mt-5">
+      <h2>Sortable List</h2>
+      <ul id="sortable-list" class="list-group">
+              <li class="list-group-item">item1</li>
+              <li class="list-group-item">item2</li>
+              <li class="list-group-item">item3</li>
+      </ul>
+    </div>
+```
+
+
+
+# Running
+
+```bash
+flask --app main --debug run
+```
+
+
+# External Links
+
+# requirements.txt
+
+# TODOs
+
---
+++ 81-90/89/main.py
@@ -0,0 +1,80 @@
+# This is my TODO app.
+from flask import Flask, render_template, redirect
+from flask_bootstrap import Bootstrap5
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired, Length, URL, NumberRange
+
+#from flask import jsonify, request
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
+from sqlalchemy import Integer, String, Boolean
+
+import requests
+from flask import request
+from flask import url_for
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///todos.db'
+app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
+# CREATE DB
+class Base(DeclarativeBase):
+    pass
+
+# Connect to Database
+db = SQLAlchemy(model_class=Base)
+db.init_app(app)
+Bootstrap5(app)
+
+# TABLE Configuration
+class Todo(db.Model):
+    __tablename__ = "todos"
+    id: Mapped[int] = mapped_column(Integer, primary_key=True)
+    title: Mapped[str] = mapped_column(String(250), unique=True, nullable=False)
+    description: Mapped[str] = mapped_column(String(500), nullable=False)
+
+    def to_dict(self):
+        return {column.name: getattr(self, column.name) for column in self.__table__.columns}
+
+with app.app_context():
+    db.create_all()
+
+
+class TodoForm(FlaskForm):
+    title = StringField(
+        'Todo title',
+        validators=[DataRequired(), Length(min=2, max=50)]
+    )
+    description = StringField(
+        'Todo description',
+        validators=[DataRequired(), Length(min=2, max=250)]
+    )
+    submit = SubmitField('Submit')
+
+# all Flask routes below
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+# all Flask routes below
+@app.route("/todos")
+def todos():
+    result = db.session.execute(db.select(Todo).order_by(Todo.title))
+    todos_all = result.scalars().all()
+    todos_list=[todo for todo in todos_all]
+    # print(f'my list is {todos_list}')
+    return render_template('todos.html', todos=todos_list)
+
+# HTTP DELETE - Delete Record
+@app.route("/delete/<int:todo_id>", methods=['GET', 'DELETE'])
+def delete(todo_id):
+    print(f'Trying to delete {todo_id}')
+    todo = db.session.get(Todo, todo_id)
+    if todo:
+        db.session.delete(todo)
+        db.session.commit()
+        print (f"Successfully deleted cafe with id {todo_id}.")
+    else:
+        print(f"Sorry a cafe with that id {todo_id} was not found in the database.")
+
+    return redirect(url_for('todos'))
