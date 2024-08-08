133f76b9059db4e4d6027973d4bf0a9663ca6bbb
Marco Morales <marco@sportsensesay.com>
2024-08-07 21:50:28 -0400

    We use the API to get random, get all, and add a cafe.

--- 81-90/88/front-end/main.py
+++ 81-90/88/front-end/main.py
@@ -4,9 +4,13 @@ from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
 from wtforms.validators import DataRequired, Length, URL, NumberRange
 
+#from flask import jsonify, request
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
+from sqlalchemy import Integer, String, Boolean
+
 import requests
 from flask import request
-import csv
 from flask import url_for
 
 '''
@@ -23,39 +27,78 @@ This will install the packages from requirements.txt for this project.
 '''
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///cafes.db'
 app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
+
+# CREATE DB
+class Base(DeclarativeBase):
+    pass
+
+# Connect to Database
+db = SQLAlchemy(model_class=Base)
+db.init_app(app)
 Bootstrap5(app)
 
+with app.app_context():
+    db.create_all()
+
+# Cafe TABLE Configuration
+class Cafe(db.Model):
+    id: Mapped[int] = mapped_column(Integer, primary_key=True)
+    name: Mapped[str] = mapped_column(String(250), unique=True, nullable=False)
+    map_url: Mapped[str] = mapped_column(String(500), nullable=False)
+    img_url: Mapped[str] = mapped_column(String(500), nullable=False)
+    location: Mapped[str] = mapped_column(String(250), nullable=False)
+    seats: Mapped[str] = mapped_column(String(250), nullable=False)
+    has_toilet: Mapped[bool] = mapped_column(Boolean, nullable=False)
+    has_wifi: Mapped[bool] = mapped_column(Boolean, nullable=False)
+    has_sockets: Mapped[bool] = mapped_column(Boolean, nullable=False)
+    can_take_calls: Mapped[bool] = mapped_column(Boolean, nullable=False)
+    coffee_price: Mapped[str] = mapped_column(String(250), nullable=True)
+
+    def to_dict(self):
+        return {column.name: getattr(self, column.name) for column in self.__table__.columns}
 
 class CafeForm(FlaskForm):
     name = StringField(
         'Cafe name',
         validators=[DataRequired(), Length(min=2, max=50)]
     )
-    location = StringField(
+    map_url = StringField(
         'Map link',
         validators=[DataRequired(), URL(require_tld=False, message='Invalid URL')]
     )
-    open_time = StringField(
-        'Opening Time',
+    img_url = StringField(
+        'Image link',
+        validators=[DataRequired(), URL(require_tld=False, message='Invalid URL')]
+    )
+    location = StringField(
+        'Location',
+        validators=[DataRequired(), Length(min=2, max=250)]
+    )
+    seats = StringField(
+        'Seats',
         validators=[DataRequired()]
     )
-    close_time = StringField(
-        'Closing Time',
+    has_toilet = StringField(
+        'Toilet Availability',
         validators=[DataRequired()]
     )
-    wifi_rating = StringField(
-        'Wifi Rating',
-        validators=[DataRequired(), Length(min=0, max=10)]
-        # TODO: Figure out how to use NumberRange
+    has_wifi = StringField(
+        'Wifi Availability',
+        validators=[DataRequired()]
     )
-    coffee_rating = StringField(
-        'Coffee Rating',
-        validators=[DataRequired(), Length(min=0, max=10)]
+    has_sockets = StringField(
+        'Sockets Availability',
+        validators=[DataRequired()]
     )
-    power_rating = StringField(
-        'Power Rating',
-        validators=[DataRequired(), Length(min=0, max=10)]
+    can_take_calls = StringField(
+        'Can Take Calls',
+        validators=[DataRequired()]
+    )
+    coffee_price = StringField(
+        'Coffee Price',
+        validators=[DataRequired()]
     )
     submit = SubmitField('Submit')
 
@@ -80,31 +123,49 @@ def add_cafe():
 
     if form.validate_on_submit():
         print('Form validated')
-        # Exercise:
-        # Make the form write a new row into cafe-data.csv
-        # with   if form.validate_on_submit()
-        # TOOD: use an emoji instead of a rating number.
-        with open('cafe-data.csv', 'a', newline='', encoding='utf-8') as data_file:
-            data_file.write(f"{form.name.data},"
-                            f"{form.location.data},"
-                            f"{form.open_time.data},"
-                            f"{form.close_time.data},"
-                            f"{form.coffee_rating.data},"
-                            f"{form.wifi_rating.data},"
-                            f"{form.power_rating.data}\n")
+        new_cafe = Cafe(
+            name=request.form.get("name"),
+            map_url=request.form.get("map_url"),
+            img_url=request.form.get("img_url"),
+            location=request.form.get("location"),
+            has_sockets=bool(request.form.get("sockets")),
+            has_toilet=bool(request.form.get("toilet")),
+            has_wifi=bool(request.form.get("wifi")),
+            can_take_calls=bool(request.form.get("calls")),
+            seats=request.form.get("seats"),
+            coffee_price=request.form.get("coffee_price"),
+        )
+        db.session.add(new_cafe)
+        db.session.commit()
         return redirect(url_for('cafes'))
     else:
         return render_template('add.html', form=form)
 
-
 @app.route('/cafes')
 def cafes():
-    with open('cafe-data.csv', newline='', encoding='utf-8') as csv_file:
-        csv_data = csv.reader(csv_file, delimiter=',')
-        list_of_rows = []
-        for row in csv_data:
-            list_of_rows.append(row)
-    return render_template('cafes.html', cafes=list_of_rows)
+    result = db.session.execute(db.select(Cafe).order_by(Cafe.name))
+    all_cafes = result.scalars().all()
+    mylist=[cafe for cafe in all_cafes]
+    print(f'my list is {mylist}')
+    return render_template('cafes.html', cafes=mylist)
+
+@app.route("/random", methods=["GET"])
+def get_random_cafe():
+    random_cafe = db.session.execute(db.select(Cafe).order_by(db.func.random())).scalar()
+    print (random_cafe)
+    return render_template('cafe.html', cafe=random_cafe)
+
+
+# HTTP DELETE - Delete Record
+@app.route("/delete/<int:cafe_id>", methods=["DELETE"])
+def delete_cafe(cafe_id):
+    cafe = db.session.get(Cafe, cafe_id)
+    if cafe:
+        db.session.delete(cafe)
+        db.session.commit()
+        print (f"Successfully deleted cafe with id {cafe_id}.")
+    else:
+        print(f"Sorry a cafe with that id {cafe_id} was not found in the database.")
 
 
 if __name__ == '__main__':
