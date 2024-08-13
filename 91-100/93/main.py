3b3f507b1ccbc7acd979ee12b1971976c76830c0
Marco Morales <marco@sportsensesay.com>
2024-08-13 15:21:19 -0400

    done.

--- 91-100/93/main.py
+++ 91-100/93/main.py
@@ -42,7 +42,10 @@ print (f'Title is {soup.title.text}')
 
 # Start by getting the row headings
 # /html/body/div[1]/div[2]/div[2]/div[3]/section[2]/div/div[2]/div[3]/table/thead/tr
-
+# document.querySelector("#__next > div.Layout_base__6IeUC.Layout_justNav__2H4H0.Layout_withSubNav__ByKRF > div.Layout_mainContent__jXliI > div.MaxWidthContainer_mwc__ID5AG > section.Block_block__62M07.nba-stats-content-block > div > div.Crom_base__f0niE > div.Crom_container__C45Ti.crom-container > table > thead > tr")
+# #__next > div.Layout_base__6IeUC.Layout_justNav__2H4H0.Layout_withSubNav__ByKRF > div.Layout_mainContent__jXliI > div.MaxWidthContainer_mwc__ID5AG > section.Block_block__62M07.nba-stats-content-block > div > div.Crom_base__f0niE > div.Crom_container__C45Ti.crom-container > table > thead > tr
+# <tr class="Crom_headers__mzI_m"><th field="RANK" class="Crom_text__NpR1_ Crom_stickyRank__aN66p">#</th><th field="PLAYER_NAME" class="Crom_text__NpR1_ Crom_primary__EajZu Crom_stickySecondColumn__29Dwf uppercase">Player</th><th field="TEAM" class="Crom_text__NpR1_">TEAM</th><th field="GP" title="Games Played">GP</th><th field="MIN" title="Minutes Played">MIN</th><th field="PTS" title="Points">PTS</th><th field="FGM" title="Field Goals Made">FGM</th><th field="FGA" title="Field Goals Attempted">FGA</th><th field="FG_PCT" title="Field Goal Percentage">FG%</th><th field="FG3M" title="3 Point Field Goals Made">3PM</th><th field="FG3A" title="3 Point Field Goals Attempted">3PA</th><th field="FG3_PCT" title="3 Point Field Goal Percentage">3P%</th><th field="FTM" title="Free Throws Made">FTM</th><th field="FTA" title="Free Throws Attempted">FTA</th><th field="FT_PCT" title="Free Throw Percentage">FT%</th><th field="OREB" title="Offensive Rebounds">OREB</th><th field="DREB" title="Defensive Rebounds">DREB</th><th field="REB" title="Rebounds">REB</th><th field="AST" title="Assists">AST</th><th field="STL" title="Steals">STL</th><th field="BLK" title="Blocks">BLK</th><th field="TOV" title="Turnovers">TOV</th><th field="EFF">EFF</th></tr>
+ 
 csv_row = []
 nba_data = []
 row_names = soup.select(selector="html body div:first-child div:nth-of-type(2) div:nth-of-type(2) div:nth-of-type(3) section:nth-of-type(2) div div:nth-of-type(2) div:nth-of-type(3) table thead tr th")
