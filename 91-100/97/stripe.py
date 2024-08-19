13ef0b8f426f6932adfd37a70cdf307cdc285e42
Marco Morales <marco@sportsensesay.com>
2024-08-18 20:41:20 -0400

    placeholder for stripe payments.

---
+++ 91-100/97/stripe.py
@@ -0,0 +1,49 @@
+#! /usr/bin/env python3.6
+"""
+Python 3.6 or newer required.
+"""
+import json
+import os
+import stripe
+
+# Read the strip api key from the environment variable STRIPE_SECRET_KEY
+stripe.api_key = os.environ().get('STRIPE_SECRET_KEY')
+
+from flask import Flask, render_template, jsonify, request
+
+
+app = Flask(__name__, static_folder='public',
+            static_url_path='', template_folder='public')
+
+def calculate_order_amount(items):
+    cart_items = db.session.query(Cart).filter_by(user_id=current_user.id).all()
+    print (f"Number of cart items is {len(cart_items)}")
+    total_price = sum(item.price * item.quantity for item in cart_items)
+    # Replace this constant with a calculation of the order's amount
+    # Calculate the order total on the server to prevent
+    # people from directly manipulating the amount on the client
+    # return 1400
+    return total_price
+
+
+@app.route('/create-payment-intent', methods=['POST'])
+def create_payment():
+    try:
+        data = json.loads(request.data)
+        # Create a PaymentIntent with the order amount and currency
+        intent = stripe.PaymentIntent.create(
+            amount=calculate_order_amount(data['items']),
+            currency='usd',
+            # In the latest version of the API, specifying the `automatic_payment_methods` parameter is optional because Stripe enables its functionality by default.
+            automatic_payment_methods={
+                'enabled': True,
+            },
+        )
+        return jsonify({
+            'clientSecret': intent['client_secret']
+        })
+    except Exception as e:
+        return jsonify(error=str(e)), 403
+
+if __name__ == '__main__':
+    app.run(port=4242)
