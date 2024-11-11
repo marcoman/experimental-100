2d378d366ad4984b76507d73fd90a4a3fe0405d7
Marco Morales. <mr.marco.a.morales@gmail.com>
2024-11-11 10:57:04 -0500

    Minor changes.

--- 91-100/93/main.py
+++ 91-100/93/main.py
@@ -11,14 +11,14 @@ from lxml import etree
 
 NBA_URL = "https://www.nba.com/stats/leaders"
 
-headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:50.0) Gecko/20100101 Firefox/50.0',
-           'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
-           'Accept-Language': 'en-US,en;q=0.5',
-           'Accept-Encoding': 'gzip, deflate',
-           'Connection': 'keep-alive',
-           'Upgrade-Insecure-Requests': '1',
-           'Cache-Control': 'max-age=0'
-           }
+# headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:50.0) Gecko/20100101 Firefox/50.0',
+#            'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+#            'Accept-Language': 'en-US,en;q=0.5',
+#            'Accept-Encoding': 'gzip, deflate',
+#            'Connection': 'keep-alive',
+#            'Upgrade-Insecure-Requests': '1',
+#            'Cache-Control': 'max-age=0'
+#            }
 
 TEST_URL = NBA_URL
 
